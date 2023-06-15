7461efd1d5ec0d4d883dd10563db56d9d1728ff2
jamalaso <72360591+jamalaso@users.noreply.github.com>
2023-06-15 16:19:42 +0000

    added functional rate limit

--- server.js
+++ server.js
@@ -15,6 +15,59 @@ const { connectToDb } = require('./lib/mongo')
 const app = express()
 const port = process.env.PORT || 8000
 
+const redis = require("redis")
+
+const redisHost = process.env.REDIS_HOST || "localhost"
+const redisPort = process.env.REDIS_PORT || "6379"
+const redisClient = redis.createClient({
+  url: `redis://${redisHost}:${redisPort}`
+})
+
+const rateLimitWindowMilliseconds = 60000;
+const rateLimitWindowMaxRequests = 10;
+const rateLimitRefreshRate = rateLimitWindowMaxRequests / rateLimitWindowMilliseconds
+
+
+async function rateLimit(req, res, next) {
+    let tokenBucket
+    try {
+      tokenBucket = await redisClient.hGetAll(req.ip)
+    } catch (e) {
+      next()
+      return
+    }
+  
+    tokenBucket = {
+      tokens: parseFloat(tokenBucket.tokens) || rateLimitWindowMaxRequests,
+      last: parseInt(tokenBucket.last) || Date.now()
+    }
+  
+    const timestamp = Date.now()
+    const ellapsedMillis = timestamp - tokenBucket.last
+    tokenBucket.tokens += ellapsedMillis * rateLimitRefreshRate
+    tokenBucket.tokens = Math.min(tokenBucket.tokens, rateLimitWindowMaxRequests)
+    tokenBucket.last = timestamp
+  
+    if (tokenBucket.tokens >= 1) {
+      tokenBucket.tokens -= 1
+      await redisClient.hSet(req.ip, [
+        [ "tokens", tokenBucket.tokens ],
+        [ "last", tokenBucket.last ]
+      ])
+      next()
+    } else {
+      await redisClient.hSet(req.ip, [
+        [ "tokens", tokenBucket.tokens ],
+        [ "last", tokenBucket.last ]
+      ])
+      res.status(429).send({
+        error: "Too many requests per minute"
+      })
+    }
+  }
+  
+  app.use(rateLimit)
+
 /*
  * Morgan is a popular logger.
  */
@@ -46,8 +99,36 @@ app.use('*', function (err, req, res, next) {
     })
 })
 
+
+/*
+redisClient.connect().then(function () {
+    app.listen(port, () => {
+      console.log("== Server is running on port", port);
+    });
+  })
+  */
+
+/*
 connectToDb(function () {
     app.listen(port, function () {
         console.log("== Server is running on port", port)
     })
 })
+*/
+
+connectToDb(function () {
+    if (!redisClient.connection) {
+      redisClient.connect().then(function () {
+        app.listen(port, () => {
+          console.log("== Server is running on port", port);
+        });
+      }).catch(function (error) {
+        console.error("Error connecting to Redis:", error);
+      });
+    } else {
+      app.listen(port, () => {
+        console.log("== Server is running on port", port);
+      });
+    }
+  });
+  
