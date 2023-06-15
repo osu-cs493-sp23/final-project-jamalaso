e4eb978eab6601417e74177abfe112335c10f839
jamalaso <72360591+jamalaso@users.noreply.github.com>
2023-06-15 17:03:27 +0000

    adding 30 request rate limit for valid auth token

--- server.js
+++ server.js
@@ -26,47 +26,71 @@ const redisClient = redis.createClient({
 const rateLimitWindowMilliseconds = 60000;
 const rateLimitWindowMaxRequests = 10;
 const rateLimitRefreshRate = rateLimitWindowMaxRequests / rateLimitWindowMilliseconds
-
+const authenticatedRateLimitWindowMaxRequests = 30;
 
 async function rateLimit(req, res, next) {
-    let tokenBucket
-    try {
-      tokenBucket = await redisClient.hGetAll(req.ip)
-    } catch (e) {
-      next()
-      return
-    }
+  let tokenBucket
+  const ip = req.ip
+  const authToken = req.headers.authorization // Assuming authentication token is passed in the headers
   
-    tokenBucket = {
-      tokens: parseFloat(tokenBucket.tokens) || rateLimitWindowMaxRequests,
-      last: parseInt(tokenBucket.last) || Date.now()
-    }
-  
-    const timestamp = Date.now()
-    const ellapsedMillis = timestamp - tokenBucket.last
-    tokenBucket.tokens += ellapsedMillis * rateLimitRefreshRate
-    tokenBucket.tokens = Math.min(tokenBucket.tokens, rateLimitWindowMaxRequests)
-    tokenBucket.last = timestamp
-  
-    if (tokenBucket.tokens >= 1) {
-      tokenBucket.tokens -= 1
-      await redisClient.hSet(req.ip, [
-        [ "tokens", tokenBucket.tokens ],
-        [ "last", tokenBucket.last ]
-      ])
-      next()
+  try {
+    if (authToken) {
+      tokenBucket = await redisClient.hGetAll(`user:${authToken}`)
     } else {
-      await redisClient.hSet(req.ip, [
-        [ "tokens", tokenBucket.tokens ],
-        [ "last", tokenBucket.last ]
+      tokenBucket = await redisClient.hGetAll(`ip:${ip}`)
+    }
+  } catch (e) {
+    next()
+    return
+  }
+
+  tokenBucket = {
+    tokens: parseFloat(tokenBucket.tokens) || rateLimitWindowMaxRequests,
+    last: parseInt(tokenBucket.last) || Date.now()
+  }
+
+  const timestamp = Date.now()
+  const elapsedMillis = timestamp - tokenBucket.last
+
+  if (authToken) {
+    tokenBucket.tokens += elapsedMillis * (authenticatedRateLimitWindowMaxRequests / rateLimitWindowMilliseconds)
+    tokenBucket.tokens = Math.min(tokenBucket.tokens, authenticatedRateLimitWindowMaxRequests)
+  } else {
+    tokenBucket.tokens += elapsedMillis * rateLimitRefreshRate
+    tokenBucket.tokens = Math.min(tokenBucket.tokens, rateLimitWindowMaxRequests)
+  }
+
+  tokenBucket.last = timestamp
+
+  if (tokenBucket.tokens >= 1) {
+    tokenBucket.tokens -= 1
+    if (authToken) {
+      await redisClient.hSet(`user:${authToken}`, [
+        ["tokens", tokenBucket.tokens],
+        ["last", tokenBucket.last]
       ])
+    } else {
+      await redisClient.hSet(`ip:${ip}`, [
+        ["tokens", tokenBucket.tokens],
+        ["last", tokenBucket.last]
+      ])
+    }
+    next()
+  } else {
+    if (authToken) {
       res.status(429).send({
-        error: "Too many requests per minute"
+        error: "Too many requests per minute (per user)"
+      })
+    } else {
+      res.status(429).send({
+        error: "Too many requests per minute (per IP)"
       })
     }
   }
-  
-  app.use(rateLimit)
+}
+
+app.use(rateLimit)
+
 
 /*
  * Morgan is a popular logger.
